34d8467297d5840402da1a2cf3ddeb8b45cf777a
DOWELL AKA <wthomasss06@gmail.com>
2026-09-17 11:55:48 +0000

    r3s3ign3m3nt init

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -24,6 +24,14 @@ const withPWA = withPWAInit({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
 };
 
 export default withPWA(nextConfig);
